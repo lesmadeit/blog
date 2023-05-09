ea18e87bcc8806a7be0fb7d47d2acb983117dd7f
leslie <lesmwendwa@gmail.com>
2023-05-09 13:59:35 +0300

    models

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogapp',
     'users',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'index'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -1,6 +1,8 @@
-from django.urls import re_path
+from django.urls import re_path, include
 from .views import Index
 
 urlpatterns = [
+    re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'^$', Index.as_view(), name='index'),
+    
 ]
