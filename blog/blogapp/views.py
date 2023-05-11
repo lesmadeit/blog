2e52f75794f1ed67bab87ec276205e37e6ca53fc
leslie <lesmwendwa@gmail.com>
2023-05-11 12:04:33 +0300

    models and views

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+from .models import Article
 
-class Index(View):
-    def get(self, request):
-        return render(request, 'blogapp/index.html')
+class Index(ListView):
+    model = Article
+    queryset = Article.objects.all().order_by('-date')
+    template_name = 'blogapp/index.html'
