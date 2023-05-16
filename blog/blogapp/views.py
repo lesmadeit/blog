cc837bf4d978ec572f2b5ca35e4c342e5badfb3e
leslie <lesmwendwa@gmail.com>
2023-05-16 15:06:11 +0300

    blog_html

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Article
 
 class Index(ListView):
@@ -8,3 +8,9 @@ class Index(ListView):
     queryset = Article.objects.all().order_by('-date')
     template_name = 'blogapp/index.html'
     paginate_by = 1
+
+
+
+class DetailArticleView(DetailView):
+    model = Article
+    template_name = 'blogapp/blog_post.html'
