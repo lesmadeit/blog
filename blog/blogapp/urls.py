ab6f8a7c058742e96f77ae6a57cf2092152b6f0d
leslie <lesmwendwa@gmail.com>
2023-06-02 12:25:43 +0300

    fixed url error

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -1,9 +1,9 @@
-from django.urls import re_path, include
+from django.urls import re_path, path, include
 from .views import Index, DetailArticleView
 
 urlpatterns = [
     re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'^$', Index.as_view(), name='index'),
-    re_path('<int:pk>', DetailArticleView.as_view(), name='detail_article'),
+    path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
     
 ]
