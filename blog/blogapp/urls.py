203967f20c9e6c69017b5a4d031ccec682ff82a5
leslie <lesmwendwa@gmail.com>
2023-06-06 13:28:36 +0300

    likes feature

--- blog/blogapp/urls.py
+++ blog/blogapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import re_path, path, include
-from .views import Index, DetailArticleView
+from .views import Index, DetailArticleView, LikeArticle
 
 urlpatterns = [
     re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'^$', Index.as_view(), name='index'),
     path('<int:pk>/', DetailArticleView.as_view(), name='detail_article'),
+    path('<int:pk>/like', LikeArticle.as_view(), name='like_article')
     
 ]
