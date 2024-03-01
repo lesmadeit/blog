6a9ac33c616d3166c4f729f9e91f7ef5786eb78d
lesmadeit <lesmwendwa@gmail.com>
2024-03-01 13:20:58 +0300

    template changes

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zz1%a07*fqkvywyaft230-6rth88e$$_3wzilwxx!1up5b$b0g'
+SECRET_KEY = os.getenv('django-insecure-zz1%a07*fqkvywyaft230-6rth88e$$_3wzilwxx!1up5b$b0g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('').split('')
 
 
 # Application definition
