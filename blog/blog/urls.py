ed58e583874c9e48cdb9e9df8901da3eae2760b2
lesmadeit <lesmwendwa@gmail.com>
2025-06-26 14:41:44 +0300

    changes

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -24,5 +24,8 @@ urlpatterns = [
     re_path('', include('blogapp.urls')),
     re_path('accounts/', include('users.urls'))
 
- ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+ 
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
